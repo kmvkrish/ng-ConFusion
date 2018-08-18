ed268915406a5844bb8c37afbafb5dd456178fe9
kmvkrish <kmvkrish@gmail.com>
2018-08-18 23:11:32 +0530

    Angular Service added

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,24 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
-import {DISHES} from '../shared/dishes';
+import { DishService } from '../services/dish.service';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
+
 export class MenuComponent implements OnInit {
 
-  dishes: Dish[] = DISHES;
+  dishes: Dish[];
   selectedDish: Dish;
 
   onSelect(dish: Dish) {
       this.selectedDish = dish;
   }
 
-  constructor() { }
+  constructor(private dishService: DishService) { }
 
   ngOnInit() {
+      this.dishes = this.dishService.getDishes();
   }
 
 }
